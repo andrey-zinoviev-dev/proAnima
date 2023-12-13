c4a27701b90a26294b42902eec3d5c620ec0fb40
Andrey Zinoviev <dronis_dronis@mail.ru>
2023-12-13 06:06:27 +0300

    updated landing page

--- src/landing.jsx
+++ src/landing.jsx
@@ -21,7 +21,45 @@ export default function Landing() {
             <div className='landing__main'>
                 <Header></Header>
                 <div className="landing__content">
-                    <div className="landing__content-action">
+                    <div className='landing__content-action'>
+                        <h1>Волшебные свечи для волшебной <span>атмосферы</span></h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum voluptate, sed mollitia dolore est maxime sint, eveniet fuga illo excepturi ipsa laudantium optio assumenda labore facere quia nesciunt quae aperiam!</p>
+                        <div>
+                            <button>
+                                <p>Заказать</p>
+                                <img src={arrowPng}></img>
+                            </button>
+                            <div>
+                                <button>
+                                    <p>Назад</p>
+                                </button>
+                                <button>
+                                    <p>Вперед</p>
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+                    {/* <Spline scene='https://prod.spline.design/2ThHoVQiJBBz1cOY/scene.splinecode' /> */}
+                    <div className='landing__content-carousel'>
+                        <ul className='landing__content-carousel-ul'>
+                            <li key="0">
+                                <Spline scene='https://prod.spline.design/2ThHoVQiJBBz1cOY/scene.splinecode' />
+                            </li>
+                            <li key="1">
+                                <Spline scene='https://prod.spline.design/2ThHoVQiJBBz1cOY/scene.splinecode' />
+                            </li>
+                        </ul>
+                        <div className='landing__content-carousel-stats'>
+                            <p>
+                                <span id='current-candle'>01</span>|<span>06</span>
+                            </p>
+                            {/* <div className=''>
+                                <div></div>
+                            </div> */}
+                        </div>
+                    </div>
+                    {/* <div className="landing__content-action">
                         <div className='landing__content-action-text'>
                             <h1>Волшебные свечи для волшебной <span>атмосферы</span></h1>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum voluptate, sed mollitia dolore est maxime sint, eveniet fuga illo excepturi ipsa laudantium optio assumenda labore facere quia nesciunt quae aperiam!</p>
@@ -65,7 +103,7 @@ export default function Landing() {
                             </button>
                         </div>
                         <Spline scene="https://prod.spline.design/2ThHoVQiJBBz1cOY/scene.splinecode" />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
