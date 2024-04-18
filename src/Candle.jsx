6dc581cd9a87a7bed46666f6200c1863012cfaab
Andrey <sttrog_810@mail.ru>
2024-04-18 11:59:12 -0700

    upadted candle lyaout

--- src/Candle.jsx
+++ src/Candle.jsx
@@ -1,17 +1,20 @@
 // import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Good.css";
-import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faExclamation, faExclamationCircle, faInfo, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default function Candle({ candle, setOrder }) {
     return (
         <article className="landing__goods-ul-li-article">
+            <button className="landing__goods-ul-li-article-wrapper-info">
+                <FontAwesomeIcon icon={faInfoCircle} />
+            </button>
             <img className="landing__goods-ul-li-article-img" src={candle.cover}></img>
 
             <div className="landing__goods-ul-li-article-wrapper">
                 <h3>{candle.name}</h3>
                 {candle.type.includes("container") && <p>Объем <span style={{fontWeight: 500}}>{candle.size}</span></p>}
-                {candle.type.includes("container") && candle.size === 190 && <p><FontAwesomeIcon icon={faExclamation}></FontAwesomeIcon> У свечей этого бъема нет крышки</p>}
+                {candle.type.includes("container") && candle.size === 190 && <div className="landing__goods-ul-li-article-wrapper-warning"><FontAwesomeIcon icon={faExclamationCircle}></FontAwesomeIcon> <p>У свечей этого бъема нет крышки</p></div>}
                 {/* {candle.type.includes("container") && <ul className="landing__goods-ul-li-article-ul">
                     <li>
                         <p>250</p>
